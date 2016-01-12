0bd4e6c7d6d92eaf2098d5b8e69e965dad52184e
Dan Cusher <dan@dancusher.com>
2016-01-11 19:19:39 -0500

    WIP

--- friends/urls.py
+++ friends/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'friends.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'friends.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
