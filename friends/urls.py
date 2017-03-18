97f8c9aa89251cb59733c067d5d84ea23047a56e
Dan Cusher <dan@dancusher.com>
2017-03-18 10:11:26 -0400

    Replace view strings with view callables in urls.py

--- friends/urls.py
+++ friends/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from adminplus.sites import AdminSitePlus
+import friends.views
 
 admin.site = AdminSitePlus()
 admin.autodiscover()
@@ -10,10 +11,10 @@ urlpatterns = [
     # url(r'^$', 'friends.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'friends.views.home', name='home'),
-    url(r'^about/$', 'friends.views.about', name='about'),
-    url(r'^contact/$', 'friends.views.contact', name='contact'),
-    url(r'^join/$', 'friends.views.join', name='join'),
-    url(r'^downunder/$', 'friends.views.downunder', name='downunder'),
+    url(r'^$', friends.views.home, name='home'),
+    url(r'^about/$', friends.views.about, name='about'),
+    url(r'^contact/$', friends.views.contact, name='contact'),
+    url(r'^join/$', friends.views.join, name='join'),
+    url(r'^downunder/$', friends.views.downunder, name='downunder'),
     url(r'^admin/', include(admin.site.urls)),
 ]
