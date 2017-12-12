246cacba4a6ad68a9e72dfe1cf6e9e786f77ff01
Dan Cusher <dan@dancusher.com>
2017-12-12 18:20:16 -0500

    Fix role order bug

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.db import models
 from django.core.validators import (
     validate_email,
@@ -105,19 +107,20 @@ class Role(models.Model):
     def __str__(self):
         return "%s, %s" % (self.member.__str__(), self.title)
     def __key__(self):
-        role_order = {
-            'president': 0,
-            'secretary': 1,
-            'treasurer': 2,
-            'director': 1000,
-            'chair': 2000,
-            'member': 3000,
-        }
+        role_order = OrderedDict([
+            ('president', 0),
+            ('secretary', 1),
+            ('treasurer', 2),
+            ('director', 1000),
+            ('chair', 2000),
+            ('member', 3000),
+        ])
         key = role_order.get(self.title.lower())
         if key is None:
             for ro in role_order:
                 if ro in self.title.lower():
                     key = role_order.get(ro) + 0.0001
+                    break
         if key is None:
             key = 1000000
         return key
