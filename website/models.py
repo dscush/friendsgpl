734be7e6e7055f2e940c71216588da23a20ef588
Dan Cusher <dan@dancusher.com>
2018-11-22 21:13:35 +0000

    Create CMSBlock model for for Summernote

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class CMSBlock(models.Model):
+    id = models.CharField(max_length=25, primary_key=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.id
