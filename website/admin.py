38d134b0270e1f249f388e0e4f54b2fcdcf03ddf
Dan Cusher <dan@dancusher.com>
2018-11-28 22:58:29 -0500

    Create admin for pages

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import CMSBlock
+from .models import CMSBlock, Page
 
 class CMSBlockAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 admin.site.register(CMSBlock, CMSBlockAdmin)
+
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Page, PageAdmin)
