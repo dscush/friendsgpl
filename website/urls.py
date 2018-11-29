e83ea820351f1ecc618e79fb9b054fef2ecca2a8
Dan Cusher <dan@dancusher.com>
2018-11-29 00:08:39 -0500

    Add TODO comment to refactor bad Page urlpatterns design

--- website/urls.py
+++ website/urls.py
@@ -21,4 +21,8 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 for page in Page.objects.all():
+    # TODO: Refactor this. This design pattern is bad. This needs to be
+    # commented out in order for the migration creating the Page model runs, and
+    # the entire app needs to be restarted in order for new page records to
+    # become available here, since this code runs only on startup.
     urlpatterns.append(url(r'^{}/$'.format(page.slug), views.page))
